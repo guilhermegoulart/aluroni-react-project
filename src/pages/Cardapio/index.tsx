089a113c93c8737203b022ed9d6c08ab8875d8fd
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-08-04 12:26:38 -0300

    creating search state, and  importing seeker component.

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo } from 'assets/logo.svg'
 
 export default function Cardapio() {
+  const [busca, setBusca] = useState('');
+
   return (
     <main>
       <nav className={styles.menu}>
@@ -12,6 +16,15 @@ export default function Cardapio() {
           Tradição e qualidade desde 1989.
         </div>
       </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>
+          Cardapio
+        </h3>
+        <Buscador 
+          pesquisa={busca} 
+          setPesquisa={setBusca}
+        />
+      </section>
     </main>
   )
 }
