11b2918298931e64fb4742adf62cf792b308e4f7
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-08-04 12:29:56 -0300

    creating the Buscador component, and using interface to declare the type of props

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,16 @@
+import styles from './Buscador.module.scss';
+
+interface BuscadorProps {
+  pesquisa: string;
+  setPesquisa: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Buscador({ pesquisa, setPesquisa }: BuscadorProps) {
+  return (
+    <div className={styles.buscador}>
+      <input 
+        value={pesquisa} 
+        onChange={event => setPesquisa(event.target.value)} />
+    </div>
+  )
+} 
