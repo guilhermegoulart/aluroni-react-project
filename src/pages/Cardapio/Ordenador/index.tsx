0886ea6915eae6a2f653a1fb48c7256b34ecc32f
Guilherme Goulart Magela <guilhermegoulartmagela@gmail.com>
2023-10-31 14:37:01 -0300

    Adding a new functionality to Ordenator Component

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import styles from "./Ordenador.module.scss"
+import styles from "./Ordenador.module.scss";
 import opcoes from "./opcoes.json";
+import classNames from "classnames";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
 export default function Ordenador() {
   const [aberto, setAberto] = useState(false);
 
   return (
-    <button className={styles.ordenador} onClick={() => setAberto(!aberto)}>
+    <button
+      className={styles.ordenador}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}
+    >
       <span>Ordenar Por</span>
-      <div className={styles.ordenador__options}>
+      {aberto ? (
+        <MdKeyboardArrowUp size={20} />
+      ) : (
+        <MdKeyboardArrowDown size={20} />
+      )}
+      <div
+        className={classNames({
+          [styles.ordenador__options]: true,
+          [styles["ordenador__options--ativo"]]: aberto,
+        })}
+      >
         {opcoes.map((opcao) => (
           <div className={styles.ordenador__option} key={opcao.value}>
             {opcao.nome}
